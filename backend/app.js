3b66c9d5afb24dc429a47717be6d56766baccfbe
SerenaMarina <ngatchous18@gmail.com>
2024-11-28 22:27:15 +0100

    new

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const mongoURI = process.env.MONGO_URL || "YOUR_MONGO_DB_CONNECTION_STRING";
+const mongoURI = process.env.MONGO_URL || "mongodb+srv://serenamarina20:serena@cluster0.l3vjm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 mongoose.connect(mongoURI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
